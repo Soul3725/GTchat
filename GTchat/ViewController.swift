1f0784ef57ed2cfead440b939c266f24a7ba31c4
Soul3725 <d.crystal3725@gmail.com>
2019-11-09 05:57:22 +0900

    Initial Commit

---
+++ GTchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GTchat
+//
+//  Created by 佐藤大地 on 2019/11/09.
+//  Copyright © 2019 Soul3725. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
